78655a0d64b8156c7199777f4326c21a7b17e946
徐洪波 <xuhb-c@glodon.com>
2019-11-12 19:19:20 +0800

    统一存储远程接口地址

---
+++ Scripts/hostLib.js
@@ -0,0 +1,10 @@
+﻿/**
+ * 统一配置IP地址以及接口地址
+ **/
+
+var ip = 'http://10.1.73.13:8079';
+var hostAddr = {
+    system_config: ip + '/api/system/config/',
+    scene: ip + '/api/scene/',
+    authority: ip + '/api/appkey/'
+};
