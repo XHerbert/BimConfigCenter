8e96ef938d22506b19aa1b863c333904c840f686
徐洪波 <xuhb-c@glodon.com>
2019-11-15 16:51:42 +0800

    设备属性分组信息配置

--- Scripts/hostLib.js
+++ Scripts/hostLib.js
@@ -1,10 +1,17 @@
 ﻿/**
  * 统一配置IP地址以及接口地址
+ * 以及常用常量
  **/
 
 var ip = 'http://10.1.73.13:8079';
 var hostAddr = {
     system_config: ip + '/api/system/config/',
     scene: ip + '/api/scene/',
-    authority: ip + '/api/appkey/'
+    authority: ip + '/api/appkey/',
+    equPropGroup: ip +'/api/equPropGroup/'
+};
+
+var method = {
+    GET: "GET",
+    POST: "POST"
 };
